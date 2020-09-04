b30fb0f6dc1a9c51ddc86f4b4422316869120cc1
vedusai <70750862+vedusai@users.noreply.github.com>
2020-09-04 17:31:55 +0530

    Add files via upload

---
+++ Question solutions.js
@@ -0,0 +1,22 @@
+//program to search any character in a string
+let name="learn javascript"
+console.log(name.indexOf('e'))
+console.log(name.indexOf('a'))
+
+// program to convert minutes to seconds
+let min= 3;
+const sec= 60*min;
+console.log(sec)
+
+//program to search for a element in array of stings
+
+let arrays=["sahil","rohit","abhishek","kunal"];
+for(let i=0; i<=arrays.length-1 ; i++){
+    console.log(arrays[i]);
+}
+// program to display elements containing 'a' in them from array.
+
+
+
+// array in reverse
+console.log(arrays.slice(-1,-3));
